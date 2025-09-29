8072387138c1b39dac44b889c2acabaad1cb3f5b
DoDAON <devdaon500@gmail.com>
2025-09-30 00:21:11 +0900

    위치 미세 조정

--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -108,7 +108,7 @@
           {#if currentQuestionIndex > 0}
             <button
               on:click={goToPreviousQuestion}
-              class="px-3 py-2 rounded-lg bg-white text-green-700 hover:bg-green-50 transition"
+              class="px-3 py-2 rounded-lg bg-white text-green-700 hover:bg-green-50 transition relative -left-6"
             >
               ← 이전으로
             </button>
